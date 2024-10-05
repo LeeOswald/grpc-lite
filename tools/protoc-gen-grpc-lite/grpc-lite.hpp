5951005ec31af818619c9189c2f0cc2abb5b9889
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2024-10-05 17:32:06 +0300

    protoc plugin added

---
+++ tools/protoc-gen-grpc-lite/grpc-lite.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <google/protobuf/compiler/code_generator.h>
+
+#include <cstdint>
+#include <string>
+
+class Grpcxx 
+    : public google::protobuf::compiler::CodeGenerator 
+{
+public:
+    ~Grpcxx() 
+    {}
+
+    Grpcxx() 
+    {}
+
+    bool Generate(
+        const google::protobuf::FileDescriptor* file, 
+        const std::string& parameter, 
+        google::protobuf::compiler::GeneratorContext* context, 
+        std::string* error
+    ) const override;
+
+    uint64_t GetSupportedFeatures() const override { return FEATURE_PROTO3_OPTIONAL; }
+};
---
+++ tools/protoc-gen-grpc-lite/main.cpp
@@ -0,0 +1,9 @@
+#include "grpc-lite.hpp"
+
+#include <google/protobuf/compiler/plugin.h>
+
+int main(int argc, char *argv[]) 
+{
+    Grpcxx g;
+    return google::protobuf::compiler::PluginMain(argc, argv, &g);
+}
