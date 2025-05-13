9b7702a894c5b261ad7425bec97e952cde6f6caa
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2025-05-13 08:52:53 +0300

    connection

--- include/grpc-lite/asio/detail/connection.hxx
+++ include/grpc-lite/asio/detail/connection.hxx
@@ -1,10 +1,65 @@
 #pragma once
 
+#include <grpc-lite/http2/detail/session.hxx>
+#include <grpc-lite/detail/request.hxx>
+#include <grpc-lite/detail/response.hxx>
+
+
 #include <boost/asio/awaitable.hpp>
 #include <boost/asio/ip/tcp.hpp>
 
+#include <forward_list>
+#include <unordered_map>
+
+
 namespace grpc_lite::asio::detail
 {
+
+class GRPC_LITE_EXPORT connection 
+{
+public:
+    using requests_t = std::forward_list<::grpc_lite::detail::request>;
+    using streams_t = std::unordered_map<int32_t, ::grpc_lite::detail::request>;
+
+    connection(const connection &) = delete;
+    connection(boost::asio::ip::tcp::socket&& sock) noexcept;
+
+    operator bool() const noexcept 
+    { 
+        return !m_eos; 
+    }
+
+    boost::asio::awaitable<requests_t> reqs() noexcept;
+    boost::asio::awaitable<void> write(::grpc_lite::detail::response resp) noexcept;
+
+private:
+    template <std::size_t N> 
+    class buffer_t 
+    {
+    public:
+        constexpr char *data() noexcept 
+        { 
+            return &_data[0]; 
+        }
+
+        constexpr std::size_t capacity() const noexcept 
+        { 
+            return N; 
+        }
+
+    private:
+        char _data[N];
+    };
+
+    requests_t read(std::size_t n);
+    boost::asio::awaitable<void> write();
+
+    buffer_t<1024> m_buffer; // FIXME: make size configurable
+    bool m_eos = false;
+    http2::detail::session m_session;
+    streams_t m_streams;
+    boost::asio::ip::tcp::socket m_socket;
+};
 
 
 } // namespace grpc_lite::asio::detail {}
--- src/grpc-lite/asio/detail/connection.cxx
+++ src/grpc-lite/asio/detail/connection.cxx
@@ -1,8 +1,125 @@
 #include <grpc-lite/asio/detail/connection.hxx>
 
+#include <boost/asio/as_tuple.hpp>
+#include <boost/asio/use_awaitable.hpp>
+#include <boost/asio/write.hpp>
+
 
 namespace grpc_lite::asio::detail
 {
 
+connection::connection(boost::asio::ip::tcp::socket&& sock) noexcept 
+    : m_socket(std::move(sock)) 
+{
+}
+
+connection::requests_t connection::read(std::size_t n) 
+{
+    requests_t reqs;
+    for (auto &ev : m_session.read({ m_buffer.data(), n })) 
+    {
+        if (ev.stream_id <= 0) 
+        {
+            continue;
+        }
+
+        if (ev.type == http2::detail::event::type_t::stream_close) 
+        {
+            m_streams.erase(ev.stream_id);
+            continue;
+        }
+
+        streams_t::iterator it = m_streams.emplace(ev.stream_id, ev.stream_id).first;
+        auto& req = it->second;
+
+        switch (ev.type) 
+        {
+        case http2::detail::event::type_t::stream_data: 
+        {
+            req.read(ev.data);
+            break;
+        }
+
+        case http2::detail::event::type_t::stream_end: 
+        {
+            reqs.push_front(std::move(req));
+            m_streams.erase(ev.stream_id);
+            break;
+        }
+
+        case http2::detail::event::type_t::stream_header: 
+        {
+            req.header(std::move(ev.header->name), std::move(ev.header->value));
+            break;
+        }
+
+        default:
+            break;
+        }
+    }
+
+    return reqs;
+}
+
+boost::asio::awaitable<connection::requests_t> connection::reqs() noexcept 
+{
+    auto [ec, n] = co_await m_socket.async_read_some(
+        boost::asio::buffer(m_buffer.data(), m_buffer.capacity()), 
+        boost::asio::as_tuple(boost::asio::use_awaitable)
+    );
+
+    if (ec) 
+    {
+        // TODO: handle errors
+        m_eos = true;
+        co_return requests_t{};
+    }
+
+    requests_t reqs;
+    try 
+    {
+        reqs = read(n);
+    }
+    catch (std::exception&) 
+    {
+        // TODO: handle errors
+        m_eos = true;
+        co_return requests_t{};
+    }
+
+    co_await write();
+    co_return reqs;
+}
+
+boost::asio::awaitable<void> connection::write() 
+{
+    for (auto chunk = m_session.pending(); chunk.size() > 0; chunk = m_session.pending()) 
+    {
+        co_await boost::asio::async_write(m_socket, boost::asio::buffer(chunk), boost::asio::use_awaitable);
+    }
+}
+
+boost::asio::awaitable<void> connection::write(::grpc_lite::detail::response resp) noexcept 
+{
+    m_session.headers(
+        resp.id(),
+        {
+            {":status", "200"},
+            {"content-type", "application/grpc"},
+        });
+
+    m_session.data(resp.id(), resp.bytes());
+    co_await write();
+
+    const auto &status = resp.status();
+    m_session.trailers(
+        resp.id(),
+        {
+            {"grpc-status", status},
+            {"grpc-status-details-bin", status.details()},
+        });
+
+    co_await write();
+}
 
 } // namespace grpc_lite::asio::detail {}
